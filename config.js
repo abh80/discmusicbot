cdcaa4d9152c7580c297777f7c49b12a04719dd9
Sfue <50198413+abh80@users.noreply.github.com>
2020-05-19 16:14:12 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  token: "BOT_TOKEN",
+  youtube: "YOUTUBE_API_KEY",
+  admin: ["OWNER_ID"],
+  debug: false,
+  logger: {
+    id: "WEBHOOK_ID",
+    token: "WEBHOOK_TOKEN"
+  }
+};
